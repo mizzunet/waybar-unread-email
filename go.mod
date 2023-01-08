054b645d8c66160bcf4c2dd9b769dbe6baead5ea
John Long <xadhatter@pm.me>
2023-01-08 11:19:40 -0500

    added support for multiple servers and encryption

--- go.mod
+++ go.mod
@@ -1,10 +1,14 @@
-module unread-count
+module github.com/mizzunet/waybar-unread-email
 
 go 1.18
 
-require github.com/emersion/go-imap v1.2.0
+require (
+	github.com/emersion/go-imap v1.2.0
+	sigs.k8s.io/yaml v1.3.0
+)
 
 require (
 	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,69 +1,139 @@
 package main
 
 import (
-    "os"
-//	"log"
-	"github.com/emersion/go-imap/client"
-	"github.com/emersion/go-imap"
-    "flag"
-    "fmt"
-)
-var (
-    SERVER *string
-    USER *string
-    PASS *string
-    ICON *string
-)
-func init() {
-    SERVER   = flag.String("S","","Server")
-    USER     = flag.String("U","","Username")
-    PASS     = flag.String("P","","Password")
-    ICON     = flag.String("I","󰗰","Icon")
+	"crypto/tls"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
+	"github.com/emersion/go-imap"
+	"github.com/emersion/go-imap/client"
+	"sigs.k8s.io/yaml"
+)
+
+type Config struct {
+	ShowZero bool           `json:"showZero"`
+	Servers  []ServerConfig `json:"servers,omitempty"`
+}
+
+type ServerConfig struct {
+	Name       string `json:"name,omitempty"`
+	Address    string `json:"address,omitempty"`
+	Username   string `json:"username,omitempty"`
+	Password   string `json:"password,omitempty"`
+	Encryption string `json:"encryption,omitempty"`
+	SkipVerify bool   `json:"skipVerify,omitempty"`
+}
+
+type WaybarOutput struct {
+	Text       string `json:"text"`
+	Tooltip    string `json:"tooltip,omitempty"`
+	Class      string `json:"class,omitempty"`
+	Percentage int    `json:"percentage"`
 }
 
 func main() {
-    //const ICON_UNREAD="󰗰"
-
-    flag.Parse()
-
-	// Connect to server
-	c, err := client.Dial(*SERVER)
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
-//		log.Fatal(err)
-        fmt.Println(err)
-        os.Exit(0)
-	}
-    
-	// Login
-	if err := c.Login(*USER, *PASS); err != nil {
-//		log.Fatal(err)
-        fmt.Println(err)
-        os.Exit(0)
+		fmt.Printf("error finding user's home directory: %v", err)
+		os.Exit(1)
 	}
 
-    _, err = c.Select("INBOX", false)
-    if err !=nil {
-//      log.Fatal(err)
-        fmt.Println(err)
-        os.Exit(0)
-    }
+	var confPath, outputFmt string
+	flag.StringVar(&confPath, "config", homeDir+"/.config/waybar-unread-email/config.yaml", "path to YAML file containing configuration")
+	flag.StringVar(&outputFmt, "output", "json", "output format, one of: \"json\", \"yaml\", \"num\"")
+	flag.Parse()
 
-    // Set search criteria
-    criteria := imap.NewSearchCriteria()
-    criteria.WithoutFlags = []string{imap.SeenFlag}
-    COUNT, err := c.Search(criteria)
-    if err != nil {
-//     log.Fatal(err)
-        fmt.Println(err)
-        os.Exit(0)
-    }
+	content, err := os.ReadFile(confPath)
+	if err != nil {
+		fmt.Printf("error reading config file: %v", err)
+		os.Exit(1)
+	}
 
-    if len(COUNT) > 0 {
-    fmt.Println(`{"text":" `,len(COUNT), ICON ,`", "class": ["unread"]}` )
-    } else {
-        os.Exit(0)
-    }
+	config := &Config{}
+	yaml.Unmarshal(content, config)
+	if err != nil {
+		fmt.Printf("error unmarshaling config file: %v", err)
+		os.Exit(1)
+	}
 
-	defer c.Logout()
+	tooltips := []string{}
+	totalUnread := 0
+	for _, srv := range config.Servers {
+		enc := strings.ToLower(srv.Encryption)
+		if enc == "" {
+			enc = "no"
+		}
+
+		var err error
+		var imapClt *client.Client
+		if enc == "tls" {
+			imapClt, err = client.DialTLS(srv.Address, &tls.Config{InsecureSkipVerify: srv.SkipVerify})
+		} else {
+			imapClt, err = client.Dial(srv.Address)
+		}
+		if err != nil {
+			fmt.Printf("error dialing server '%s': %v", srv.Name, err)
+			os.Exit(1)
+		}
+		if enc == "starttls" {
+			err = imapClt.StartTLS(&tls.Config{InsecureSkipVerify: srv.SkipVerify})
+			if err != nil {
+				fmt.Printf("error dialing server '%s': %v", srv.Name, err)
+				os.Exit(1)
+			}
+		}
+		defer imapClt.Logout()
+
+		if err := imapClt.Login(srv.Username, srv.Password); err != nil {
+			fmt.Printf("error logging into server '%s': %v", srv.Name, err)
+			os.Exit(1)
+		}
+
+		_, err = imapClt.Select("INBOX", true)
+		if err != nil {
+			fmt.Printf("error selecting INBOX on server '%s': %v", srv.Name, err)
+			os.Exit(1)
+		}
+
+		// Criteria for unread emails.
+		criteria := imap.NewSearchCriteria()
+		criteria.WithoutFlags = []string{imap.SeenFlag}
+		seqNums, err := imapClt.Search(criteria)
+		if err != nil {
+			fmt.Printf("error searching INBOX for unread messages on server '%s': %v", srv.Name, err)
+			os.Exit(1)
+		}
+
+		count := len(seqNums)
+		tooltips = append(tooltips, fmt.Sprintf("%s: %d unread", srv.Name, count))
+		totalUnread += count
+	}
+
+	wbOut := &WaybarOutput{Tooltip: strings.Join(tooltips, "\n")}
+	if totalUnread > 0 {
+		wbOut.Text = strconv.Itoa(totalUnread)
+		wbOut.Class = "unread"
+		wbOut.Percentage = 100
+	} else if config.ShowZero {
+		wbOut.Text = "0"
+	}
+
+	out := ""
+	switch strings.ToLower(outputFmt) {
+	case "num":
+		out = wbOut.Text
+	case "yaml":
+		str, _ := yaml.Marshal(wbOut)
+		out = string(str)
+	default:
+		str, _ := json.Marshal(wbOut)
+		out = string(str)
+	}
+
+	fmt.Println(out)
+	os.Exit(0)
 }
